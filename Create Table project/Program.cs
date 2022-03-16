71ebcc514abb86866745c0a4fd9167cfc122d5de
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-03-16 14:47:52 +0700

    menambahkan tabel

--- Create Table project/Program.cs
+++ Create Table project/Program.cs
@@ -16,9 +16,21 @@ namespace Create_Table_project
             {
                 con = new SqlConnection("data source=LAPTOP-OG8P1VHL\\HIJJAJ;database=ProdiTI;Integrated Security = TRUE");
                 con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key," +
+                    "Nama varchar(50), Alamat varchar(255), Jenis_kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
             } catch (Exception e)
             {
-
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
             }
         }
     }
